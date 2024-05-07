ef8ba7fb32ab46f0aeba37008fbe511f65fda5b5
Larry Li <larryruili@gmail.com>
2024-05-07 05:31:44 -0400

    Add Supertab widget to clicker

--- frontend/src/components/Clicker.tsx
+++ frontend/src/components/Clicker.tsx
@@ -2,9 +2,30 @@ import React from "react";
 
 interface ClickerProps {}
 
+let widget: any;
+
+window.addEventListener("supertab.event", (event: any) => {
+   switch (event.detail.type) {
+      case "ready":
+         widget = new event.detail.Widget({
+            clientId: "client.c84095e4-746e-4dd7-89df-afec1d8ff32d",
+            merchantName: "Bilbord.app"
+         });
+      break;
+      case "back_to_caller":
+         if (event.detail.accessStatus == 'access_granted') {
+          console.log("Access granted");
+         } else {
+          console.log("Buyflow exited");
+         }
+      break;
+   }
+});
+
 const Clicker: React.FC<ClickerProps> = () => {
   const alertConsole = () => {
     console.log("Clicked!");
+    widget.showPaywall();
   };
 
   return <button onClick={alertConsole}>Hello there!</button>;
